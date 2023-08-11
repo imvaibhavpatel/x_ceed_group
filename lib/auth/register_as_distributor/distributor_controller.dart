d8c5f026e881dead81943c3930bf33a9e130e11f
imvaibhavpatel <vaibhavpatel67685@gmail.com>
2023-08-11 15:42:45 +0530

    initial commit

--- lib/auth/register_as_distributor/distributor_controller.dart
+++ lib/auth/register_as_distributor/distributor_controller.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
+import 'package:xceed_group/auth/register_as_retailer/model/city_model.dart';
+import 'package:xceed_group/auth/register_as_retailer/model/countries_model.dart';
+import 'package:xceed_group/auth/register_as_retailer/model/state_model.dart';
+import 'package:xceed_group/utils/base_url.dart';
 
 class DistributorController extends GetxController{
   final retailerRegKey = GlobalKey<FormState>();
@@ -11,9 +16,6 @@ class DistributorController extends GetxController{
   TextEditingController registerRetEmailController = TextEditingController();
   TextEditingController gstNumController = TextEditingController();
   TextEditingController pinCodeController = TextEditingController();
-  TextEditingController adhaarImageController = TextEditingController();
-  TextEditingController panImageController = TextEditingController();
-  TextEditingController gstImageController = TextEditingController();
   TextEditingController addressController = TextEditingController();
   TextEditingController brandController = TextEditingController();
   TextEditingController turnOverController = TextEditingController();
@@ -21,27 +23,101 @@ class DistributorController extends GetxController{
 
 
   RxBool isCheck = false.obs;
-  RxString countryValue = "".obs;
-  RxString stateValue = "".obs;
-  RxString cityValue = "".obs;
+  RxString disAdhaarDocPath = "".obs;
+  RxString disPanDocPath = "".obs;
+  RxString disGstDocPath = "".obs;
+  RxInt countryPage = 1.obs;
+  RxInt cityPage = 1.obs;
+  RxInt statePage = 1.obs;
+  RxInt countryID = 0.obs;
+  RxInt stateID = 0.obs;
+  RxString selectedCountries = "".obs;
+  RxString selectedState = "".obs;
+  RxString selectedCity = "".obs;
+  RxList<Country> countriesList = <Country>[].obs;
+  RxList<StateName> stateList = <StateName>[].obs;
+  RxList<City> cityList = <City>[].obs;
 
 
   getAdhaarDoc() async {
     final image = await picker.pickImage(source: ImageSource.gallery);
     if (image != null) {
-      adhaarImageController.text = image.name.toString();
+      disAdhaarDocPath.value = image.name.toString();
     }
   }
   getPanCardDoc() async {
     final image = await picker.pickImage(source: ImageSource.gallery);
     if (image != null) {
-      panImageController.text = image.name.toString();
+      disPanDocPath.value= image.name.toString();
     }
   }
   getGstDoc() async {
     final image = await picker.pickImage(source: ImageSource.gallery);
     if (image != null) {
-      gstImageController.text = image.name.toString();
+      disGstDocPath.value = image.name.toString();
     }
+  }
+  getCountry() async {
+    try {
+      final response = await http.get(
+        Uri.parse(
+            "${BaseUrl.baseUrl}${BaseUrl.countriesUrl}page=${countryPage.value}&country_name="),
+      );
+      if (response.statusCode == 200) {
+        countriesList.value += countriesModelFromJson(response.body).countries;
+        if (countriesModelFromJson(response.body).totalPages !=
+            countryPage.value) {
+          countryPage.value = countryPage.value + 1;
+          getCountry();
+        }
+      } else {
+        debugPrint("some error");
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  getState(String countryID) async {
+    try {
+      final response = await http.get(Uri.parse(
+          "${BaseUrl.baseUrl}${BaseUrl.stateURl}&country_id=$countryID&page=${statePage.value}&state_name="));
+      if (response.statusCode == 200) {
+        stateList.addAll(stateModelFromJson(response.body).states);
+        if (stateModelFromJson(response.body).totalPages != statePage.value) {
+          statePage.value = statePage.value + 1;
+          getState(countryID);
+        }
+      } else {
+        debugPrint("some error");
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  getCity(String stateID) async {
+    try {
+      final response = await http.get(Uri.parse(
+          "${BaseUrl.baseUrl}${BaseUrl.cityUrl}&state_id=$stateID&page=${cityPage.value}&city_name="));
+      if (response.statusCode == 200) {
+        cityList.addAll(cityModelFromJson(response.body).cities);
+        if (cityModelFromJson(response.body).totalPages != cityPage.value) {
+          cityPage.value = cityPage.value + 1;
+          getCity(stateID);
+        }
+      } else {
+        debugPrint("some error");
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  @override
+  void onReady() {
+    // TODO: implement onReady
+    super.onReady();
+    getCountry();
   }
 }
